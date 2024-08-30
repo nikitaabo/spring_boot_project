120867136c7df54bb53aae327c436fa296763815
Nikita Boyko <nikitabojko57@gmail.com>
2024-08-30 12:02:38 +0300

    refactor Book.java file

--- src/main/java/com/example/spring/models/Book.java
+++ src/main/java/com/example/spring/models/Book.java
@@ -26,9 +26,7 @@ public class Book {
     private String isbn;
     @Column(nullable = false)
     private BigDecimal price;
-    @Column()
     private String description;
-    @Column()
     private String coverImage;
 
 }
