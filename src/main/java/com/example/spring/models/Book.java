6f0dcfadd23f3a82c1c49c4bda1ea55a745c8dd3
Nikita Boyko <nikitabojko57@gmail.com>
2024-08-30 11:34:07 +0300

    change Book.java file

--- src/main/java/com/example/spring/models/Book.java
+++ src/main/java/com/example/spring/models/Book.java
@@ -18,29 +18,17 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "title", nullable = false)
+    @Column(nullable = false)
     private String title;
-    @Column(name = "author", nullable = false)
+    @Column(nullable = false)
     private String author;
-    @Column(name = "isbn", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String isbn;
-    @Column(name = "price", nullable = false)
+    @Column(nullable = false)
     private BigDecimal price;
-    @Column(name = "description")
+    @Column()
     private String description;
-    @Column(name = "coverImage")
+    @Column()
     private String coverImage;
 
-    @Override
-    public String toString() {
-        return "Book{"
-                + "id=" + id
-                + ", title='" + title + '\''
-                + ", author='" + author + '\''
-                + ", isbn='" + isbn + '\''
-                + ", price=" + price
-                + ", description='" + description + '\''
-                + ", coverImage='" + coverImage
-                + '\'' + '}';
-    }
 }
