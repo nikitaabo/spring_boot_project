e603574809d19b5ecd59744e4a0ad83a01e816cf
Nikita Boyko <nikitabojko57@gmail.com>
2024-09-05 16:21:39 +0300

    change CreateBookRequestDto class and BookRepository interface

--- src/main/java/com/example/spring/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/spring/dto/CreateBookRequestDto.java
@@ -2,6 +2,7 @@ package com.example.spring.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -13,7 +14,7 @@ public class CreateBookRequestDto {
     private String author;
     @NotBlank
     private String isbn;
-    @NotBlank
+    @NotNull
     @Min(0)
     private BigDecimal price;
     private String description;
